9278bc9c0e884ffc6c6bd6ada1c45fdefeb7a9bc
Borhan <brborhan70@gmail.com>
2025-03-29 02:35:56 +0600

    chat web application is done

---
+++ backend/src/index.js
@@ -0,0 +1,43 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+import connectDB from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+//configuration
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+connectDB();
+connectDB();
+
+const __dirname = path.resolve();
+
+//use middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/", (req, res) => {
+  res.send("server is running");
+});
+if (process.env.NODE_ENV) {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
