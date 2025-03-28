56943159179bae14c4c128ed28c4e322bc39afe7
Borhan <brborhan70@gmail.com>
2025-03-29 03:17:06 +0600

    some update because deploy issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/message", messageRoutes);
 app.use("/", (req, res) => {
   res.send("server is running");
 });
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
